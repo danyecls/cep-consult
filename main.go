1f2be800e098daea38a7b7bdd14c575e05fdb0c3
danyecls <danyelecls@gmail.com>
2023-04-23 22:15:27 -0300

    Adicionando main e module-go

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"app/api/handle"
+	_ "app/docs"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
+)
+
+// @title API de consulta de endereço
+// @description Esta é uma API que consulta informações de endereço a partir de um CEP.
+// @version 1
+// @host localhost:8080
+// @BasePath /v1
+
+func main() {
+	r := mux.NewRouter()
+
+	r.PathPrefix("/docs/").Handler(http.StripPrefix("/docs/", http.FileServer(http.Dir("./docs"))))
+	r.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
+	r.HandleFunc("/v1/consult-address", handle.ConsultAdress).Methods("POST")
+
+	http.ListenAndServe(":8080", r)
+}
